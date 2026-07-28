f0b785e7bbddd2e6665055ed8ff7ec43e2ed59ae
fraaance <116870975+fraaance@users.noreply.github.com>
2026-07-28 10:36:09 +0200

    Add project files

---
+++ ML_Model.py
@@ -0,0 +1,4 @@
+# ML - Model
+with open("/Users/franzweisel/Documents/Systembio/Cutibacterium_granulosum_TM11/cuti_TM11_translated_cds.faa") as f:
+    counter = sum(1 for l in f if l.startswith(">"))
+    print(counter)
